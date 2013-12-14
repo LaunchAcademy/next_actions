3a93f45a6a87c5920f89f89b89374ce875a2b9c1
Dan Pickett <dan.pickett@launchware.com>
2013-12-14 14:44:02 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NextActions::Application.config.secret_key_base = '3b2eb05dd1a6386aa59b1455400ccce09b91f4fc49f8f6b13cc45a3ebad34598a90871e7327013f26953debb3e251e68ad3ef3754f6e23ad73cdd0887c548e2e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NextActions::Application.config.session_store :cookie_store, key: '_next_actions_session'
